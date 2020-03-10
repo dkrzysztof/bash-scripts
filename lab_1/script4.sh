199ceb481b4ca2f4531133ba5c98a31d21334358
Krzysztof <dydakkrzysztof@gmail.com>
2020-03-10 11:58:17 +0100

    pozdrawiam dzieciaczki

---
+++ lab_1/script4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+counter=1
+for file in `ls -S $1`
+do
+        if [ -x $file ]
+        then
+                echo "${file}.${counter}" # mv $file "${file}.${counter}"
+                counter=$((counter+1))
+        fi
+done
+
+
